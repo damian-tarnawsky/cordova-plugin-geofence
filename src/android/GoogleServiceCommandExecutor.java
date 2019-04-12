a4cc6c92fd0a1d216a76206a711d0154c60c3db4
Mark, Jason <jason.mark@aristocrat.com>
2019-04-11 17:07:13 -0700

    Fix a fighting writers problem.

--- src/android/GoogleServiceCommandExecutor.java
+++ src/android/GoogleServiceCommandExecutor.java
@@ -1,18 +1,29 @@
 package com.cowbell.cordova.geofence;
 
+import android.util.Log;
+
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.Semaphore;
 
 public class GoogleServiceCommandExecutor implements IGoogleServiceCommandListener {
     private Queue<AbstractGoogleServiceCommand> commandsToExecute;
     private boolean isExecuting = false;
+    private Semaphore semaphore = new Semaphore(1);
 
     public GoogleServiceCommandExecutor() {
         commandsToExecute = new LinkedList<AbstractGoogleServiceCommand>();
     }
 
     public void QueueToExecute(AbstractGoogleServiceCommand command) {
-        commandsToExecute.add(command);
+        try {
+            semaphore.acquire();
+            commandsToExecute.add(command);
+        } catch (Exception e) {
+            Log.e("GoogleServiceCommandExecutor", e.getMessage());
+        } finally {
+            semaphore.release();
+        }
         if (!isExecuting) ExecuteNext();
     }
 
